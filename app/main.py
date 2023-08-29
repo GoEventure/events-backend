70b10b3cdedf835cebd6d989c222926031e5818f
Brian <btotharye@gmail.com>
2023-08-29 10:10:10 -0400

    feat(commitizen): adding in commitizen

--- app/main.py
+++ app/main.py
@@ -9,12 +9,13 @@ from app.core.logger import init_logging
 from app.core.main_router import router as main_router
 from app.events import events_router
 
-load_dotenv('.env')
+load_dotenv(".env")
+api_version = "1.0.0"
 
 root_router = APIRouter()
 
-app = FastAPI(title='GoEventure Events API')
-app.add_middleware(DBSessionMiddleware, db_url=os.environ['DATABASE_URL'])
+app = FastAPI(title="GoEventure Events API", version=api_version)
+app.add_middleware(DBSessionMiddleware, db_url=os.environ["DATABASE_URL"])
 
 app.include_router(main_router)
 app.include_router(events_router)
@@ -22,8 +23,8 @@ app.include_router(root_router)
 
 init_logging()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
 
-    uvicorn.run(app, host='0.0.0.0', port=8001, log_level='debug')
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
